796cc8e9bc0ce89ea12d6abb0a47203062382483
StephanCedillo <stephan.cedillo@gmail.com>
2026-05-13 13:53:36 +0000

    UPD metodos

--- src/controllers/CarController.java
+++ src/controllers/CarController.java
@@ -8,22 +8,22 @@ public class CarController {
 
     for (int i = 0; i < cars.length; i++) {
       boolean swap = false;
-      for (int j = i; j < cars.length-1; j++) {
-        if (cars[j].getName().compareTo(cars[j + 1].getName()) > 0) {
-          Car aux = cars[j + 1];
-          cars[j + 1] = cars[j];
+      for (int j = 0; j < cars.length-1-i; j++) {
+        if (cars[j].getName().compareTo(cars[j+1].getName())>0) {
+          Car aux = cars[j];
+          cars[j+1] = cars[j];
           cars[j] = aux;
           swap = true;
-
         }
+        
 
       }
-      if (!swap) {
-        break;
 
-      }
+     
+      
 
     }
+    
 
   }
 }
